e1137568b99e373590de92c527a2b8cf463ffe9d
zhiyouzhao <zhiyouzhao@foxmail.com>
2024-10-18 15:18:14 +0800

    add test support

---
+++ assignment1/readme.md
@@ -0,0 +1,24 @@
+## Todo:
+
+- Coding
+  - [x] model
+  - [x] dataset x 3
+  - [x] data pipeline x 3
+  - [x] IoU and mIoU metric
+  - [ ] face detection based pipeline
+  - [x] training and validation script
+    - [x] lr scheduler
+    - [x] tensorboard
+    - [x] checkpoint
+  - [x] inferring script
+    - [x] img + mask visualization
+- Training
+  - [x] EG1800 + ResNet50 (Local)
+  - [x] EG1800 + Centering + ResNet50 (Local)
+  - [x] MattingHuman + ResNet50 [Kaggle](https://www.kaggle.com/code/kaggleeeeeeeeeeee/mattinghuman)
+  - [x] MattingHuman + ResNet34 [Kaggle](https://www.kaggle.com/code/kaggleeeeeeeeeeee/mattingsmall)
+  - [x] EasyPortrait + ResNet50 [Kaggle](https://www.kaggle.com/code/marcusliang12/easyportrait)
+- Extra Testing
+  - [ ] EG1800 + ResNet50 -> Realworld
+  - [ ] MattingHuman + ResNet50 -> Realworld
+  - [ ] MattingHuman + ResNet50 -> EG1800
---
+++ assignment1/task3/test.py
@@ -0,0 +1,60 @@
+import os
+from os.path import join
+
+import torch
+
+from portraitnet import build_model
+from dataset import build_data
+from utils import parse_args, load_config, Iou
+
+
+def validate(model, test_loader, metric_f, device):
+    model.eval()
+    val_metric = 0.0
+
+    with torch.no_grad():
+        for i, (images, masks) in enumerate(test_loader, 1):
+            images = images.to(device)
+            masks = masks.to(device)
+
+            outputs = model(images)
+            metric = metric_f(outputs, masks)
+
+            val_metric += metric.item()
+
+            print(f"Val Iter: [{i}/{len(test_loader)}], Iou: {metric.item():.4f}")
+
+    val_metric = val_metric / len(test_loader)
+
+    return val_metric
+
+
+def test():
+    args = parse_args()
+    config = load_config(args.config)
+
+    # work_dir
+    os.makedirs(config["work_dir"], exist_ok=True)
+
+    # device
+    device = torch.device(config["device"] if torch.cuda.is_available() else "cpu")
+
+    # model
+    model = build_model(config["model"])
+    model = model.to(device)
+    checkpoint_path = join(config["work_dir"], "best.pth")
+    model.load_state_dict(torch.load(checkpoint_path, map_location=device))
+
+    # data
+    _, test_loader = build_data(config["data"])
+
+    # metric
+    metric_f = Iou()
+
+    # testing
+    val_metric = validate(model, test_loader, metric_f, device)
+    print(f"Validation IoU: {val_metric:.4f}")
+
+
+if __name__ == "__main__":
+    test()
